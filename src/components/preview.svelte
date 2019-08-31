d506a49099e74873bce7ef918d72392331284955
alete89 <alejandrodini@mural.co>
2019-08-31 18:20:11 -0300

    feat: preview tabla component

---
+++ src/components/preview.svelte
@@ -0,0 +1,75 @@
+<script>
+  import { Table } from "sveltestrap";
+  import { onMount } from "svelte";
+  import { Alert } from "sveltestrap";
+
+  let data = [];
+  let error = "";
+  onMount(async function() {
+    const response = await fetch("http://localhost:3000/gastosx").catch(
+      e => (error = e)
+    );
+    if (response) {
+      if (response.ok) {
+        const json = await response.json();
+        data = json;
+      }
+      if (!response.ok) {
+        error = `Error ${response.status}: ${response.statusText}`;
+      }
+    }
+    console.log("el error vale: ", error);
+  });
+
+  function formatDate(ISOString) {
+    fecha = new Date(ISOString).toISOString().slice(0, 10);
+    return fecha;
+  }
+</script>
+
+<head>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+</head>
+
+<!-- Alerta para errores -->
+<Alert color="danger" isOpen={error} toggle={() => (error = false)}>
+  {error}
+</Alert>
+<Table hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Producto</th>
+      <th>Comercio</th>
+      <th>Monto total</th>
+      <th>Moneda</th>
+      <th>Cuotas</th>
+      <th>Fecha</th>
+      <th>First Month</th>
+      <th>Paga IVA?</th>
+      <th>Monto IVA</th>
+      <th>Tags</th>
+      <th>Tarjeta</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each data as gasto, index}
+      <tr>
+        <th scope="row">{index + 1}</th>
+        <td>{gasto.producto}</td>
+        <td>{gasto.comercio}</td>
+        <td>{gasto.monto_total}</td>
+        <td>{gasto.moneda.nombre}</td>
+        <td>{gasto.cuotas}</td>
+        <td>{new Date(gasto.fecha).toISOString().slice(0, 10)}</td>
+        <td>{gasto.mes_primer_resumen}</td>
+        <td>{gasto.paga_iva}</td>
+        <td>{gasto.monto_iva}</td>
+        <td>{gasto.tags.map(g => g.nombre)}</td>
+        <td>{gasto.tarjeta.nombre}</td>
+      </tr>
+    {/each}
+  </tbody>
+</Table>
