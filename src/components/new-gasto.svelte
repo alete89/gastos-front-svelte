ea5839f68dcdcf5d09a1b875568b12503505c732
nicovio <nicolasviotti90@gmail.com>
2019-09-03 21:16:00 -0300

    feat: some css changes

--- src/components/new-gasto.svelte
+++ src/components/new-gasto.svelte
@@ -63,99 +63,129 @@
   }
 </script>
 
+<style>
+  .centrado {
+    margin: auto;
+    margin-bottom:2rem;
+    margin-top:4rem;
+    width: 50% !important;
+  }
+  .titulo {
+    text-align: center;
+    font-size: 2rem;
+    font-weight: bold;
+    background-color: black;
+    color:white;
+  }
+
+  .margen {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    margin-left: 5rem;
+    margin-right: 5rem;
+  }
+</style>
+
 <head>
   <link
     rel="stylesheet"
     href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
 </head>
 
-<Form>
-
-  <FormGroup>
-    <Label for="producto">Producto</Label>
-    <Input
-      type="text"
-      name="producto"
-      id="producto"
-      placeholder="Producto"
-      bind:value={gasto.producto} />
-  </FormGroup>
-  <FormGroup>
-    <Label for="comercio">Comercio</Label>
-    <Input
-      type="text"
-      name="comercio"
-      id="comercio"
-      placeholder="Comercio"
-      bind:value={gasto.comercio} />
-  </FormGroup>
-  <FormGroup>
-    <Label for="monto_total">Monto total</Label>
-    <Input
-      type="number"
-      name="monto_total"
-      id="monto_total"
-      placeholder="Monto total"
-      bind:value={gasto.monto_total} />
-  </FormGroup>
-  <FormGroup>
-    <Label for="cuotas">Cuotas</Label>
-    <Input
-      type="number"
-      name="cuotas"
-      id="cuotas"
-      placeholder="Cuotas"
-      bind:value={gasto.cuotas} />
-  </FormGroup>
-  <FormGroup>
-    <Label for="moneda">Moneda</Label>
-    <select bind:value={gasto.moneda} name="moneda" id="moneda">
-      {#each monedas as moneda}
-        <option value={moneda.id}>{moneda.nombre}</option>
-      {/each}
-    </select>
-  </FormGroup>
-  <FormGroup>
-    <Label for="exampleDate">Date</Label>
-    <Input
-      type="date"
-      name="date"
-      id="exampleDate"
-      placeholder="date placeholder"
-      bind:value={gasto.fecha} />
-  </FormGroup>
-  <FormGroup>
-    <Label for="tarjeta">Tarjeta</Label>
-    <select bind:value={gasto.tarjeta} name="tarjeta" id="tarjeta">
-      {#each tarjetas as tarjeta}
-        <option value={tarjeta.id}>{tarjeta.nombre}</option>
-      {/each}
-    </select>
-  </FormGroup>
-  <FormGroup>
-    <Label for="exampleText">Text Area</Label>
-    <Input type="textarea" name="text" id="exampleText" />
-  </FormGroup>
-  <FormGroup>
-    <input type="radio" bind:group={grupo} value="Uno" />
-    Uno
-    <input type="radio" bind:group={grupo} value="Dos" />
-    Dos
-    <input type="radio" bind:group={grupo} value="Tres" />
-    Tres
-  </FormGroup>
-  <FormGroup>
-    <input type="checkbox" bind:group={listaCheck} value="A" />
-    A
-    <input type="checkbox" bind:group={listaCheck} value="B" />
-    B
-    <input type="checkbox" bind:group={listaCheck} value="C" />
-    C
-    <input type="checkbox" bind:group={listaCheck} value="D" />
-    D
-  </FormGroup>
-  <Button color="primary" disabled={!gasto.producto} on:click={handleSubmit}>
-    Crear Gasto
-  </Button>
-  <Button color="success" on:click={handleTest}>TEST</Button>
-</Form>
+<div class="card centrado">
+  <div class="card-header titulo">Agregar gasto</div>
+  <div class="margen">
+    <Form>
+      <FormGroup>
+        <Label for="producto">Producto</Label>
+        <Input
+          type="text"
+          name="producto"
+          id="producto"
+          placeholder="Producto"
+          bind:value={gasto.producto} />
+      </FormGroup>
+      <FormGroup>
+        <Label for="comercio">Comercio</Label>
+        <Input
+          type="text"
+          name="comercio"
+          id="comercio"
+          placeholder="Comercio"
+          bind:value={gasto.comercio} />
+      </FormGroup>
+      <FormGroup>
+        <Label for="monto_total">Monto total</Label>
+        <Input
+          type="number"
+          name="monto_total"
+          id="monto_total"
+          placeholder="Monto total"
+          bind:value={gasto.monto_total} />
+      </FormGroup>
+      <FormGroup>
+        <Label for="cuotas">Cuotas</Label>
+        <Input
+          type="number"
+          name="cuotas"
+          id="cuotas"
+          placeholder="Cuotas"
+          bind:value={gasto.cuotas} />
+      </FormGroup>
+      <FormGroup>
+        <Label for="moneda">Moneda</Label>
+        <select bind:value={gasto.moneda} name="moneda" id="moneda">
+          {#each monedas as moneda}
+            <option value={moneda.id}>{moneda.nombre}</option>
+          {/each}
+        </select>
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleDate">Date</Label>
+        <Input
+          type="date"
+          name="date"
+          id="exampleDate"
+          placeholder="date placeholder"
+          bind:value={gasto.fecha} />
+      </FormGroup>
+      <FormGroup>
+        <Label for="tarjeta">Tarjeta</Label>
+        <select bind:value={gasto.tarjeta} name="tarjeta" id="tarjeta">
+          {#each tarjetas as tarjeta}
+            <option value={tarjeta.id}>{tarjeta.nombre}</option>
+          {/each}
+        </select>
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleText">Text Area</Label>
+        <Input type="textarea" name="text" id="exampleText" />
+      </FormGroup>
+      <FormGroup>
+        <input type="radio" bind:group={grupo} value="Uno" />
+        Uno
+        <input type="radio" bind:group={grupo} value="Dos" />
+        Dos
+        <input type="radio" bind:group={grupo} value="Tres" />
+        Tres
+      </FormGroup>
+      <FormGroup>
+        <input type="checkbox" bind:group={listaCheck} value="A" />
+        A
+        <input type="checkbox" bind:group={listaCheck} value="B" />
+        B
+        <input type="checkbox" bind:group={listaCheck} value="C" />
+        C
+        <input type="checkbox" bind:group={listaCheck} value="D" />
+        D
+      </FormGroup>
+      <Button
+        color="primary"
+        disabled={!gasto.producto}
+        on:click={handleSubmit}>
+        Crear Gasto
+      </Button>
+      <Button color="success" on:click={handleTest}>TEST</Button>
+    </Form>
+  </div>
+</div>
--- src/components/preview.svelte
+++ src/components/preview.svelte
@@ -6,7 +6,7 @@
   let data = [];
   let error = "";
   onMount(async function() {
-    const response = await fetch("http://localhost:3000/gastosx").catch(
+    const response = await fetch("http://localhost:3000/gastos").catch(
       e => (error = e)
     );
     if (response) {
@@ -27,6 +27,18 @@
   }
 </script>
 
+<style>
+  .table {
+    margin: auto;
+    width: 80% !important;
+    text-align: center;
+    background-color:white;
+  }
+  .thead{
+    background-color:#001f3f;
+  }
+</style>
+
 <head>
   <link
     rel="stylesheet"
@@ -37,39 +49,41 @@
 <Alert color="danger" isOpen={error} toggle={() => (error = false)}>
   {error}
 </Alert>
-<Table hover>
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Producto</th>
-      <th>Comercio</th>
-      <th>Monto total</th>
-      <th>Moneda</th>
-      <th>Cuotas</th>
-      <th>Fecha</th>
-      <th>First Month</th>
-      <th>Paga IVA?</th>
-      <th>Monto IVA</th>
-      <th>Tags</th>
-      <th>Tarjeta</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each data as gasto, index}
+<div class="table-responsive">
+  <table class="table table-bordered table-hover">
+    <thead class="thead-dark">
       <tr>
-        <th scope="row">{index + 1}</th>
-        <td>{gasto.producto}</td>
-        <td>{gasto.comercio}</td>
-        <td>{gasto.monto_total}</td>
-        <td>{gasto.moneda.nombre}</td>
-        <td>{gasto.cuotas}</td>
-        <td>{new Date(gasto.fecha).toISOString().slice(0, 10)}</td>
-        <td>{gasto.mes_primer_resumen}</td>
-        <td>{gasto.paga_iva}</td>
-        <td>{gasto.monto_iva}</td>
-        <td>{gasto.tags.map(g => g.nombre)}</td>
-        <td>{gasto.tarjeta.nombre}</td>
+        <th>#</th>
+        <th>Producto</th>
+        <th>Comercio</th>
+        <th>Monto total</th>
+        <th>Moneda</th>
+        <th>Cuotas</th>
+        <th>Fecha</th>
+        <th>First Month</th>
+        <th>Paga IVA?</th>
+        <th>Monto IVA</th>
+        <th>Tags</th>
+        <th>Tarjeta</th>
       </tr>
-    {/each}
-  </tbody>
-</Table>
+    </thead>
+    <tbody>
+      {#each data as gasto, index}
+        <tr>
+          <th scope="row">{index + 1}</th>
+          <td>{gasto.producto}</td>
+          <td>{gasto.comercio}</td>
+          <td>{gasto.monto_total}</td>
+          <td>{gasto.moneda.nombre}</td>
+          <td>{gasto.cuotas}</td>
+          <td>{new Date(gasto.fecha).toISOString().slice(0, 10)}</td>
+          <td>{gasto.mes_primer_resumen}</td>
+          <td>{gasto.paga_iva}</td>
+          <td>{gasto.monto_iva}</td>
+          <td>{gasto.tags.map(g => g.nombre)}</td>
+          <td>{gasto.tarjeta.nombre}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
