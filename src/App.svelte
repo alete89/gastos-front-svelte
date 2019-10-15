38e72f39564b2954b53a4cd8d0b301e6ce3ab747
alete89 <alejandrodini@mural.co>
2019-10-15 19:16:14 -0300

    Routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,18 @@
 <script>
-  import Preview from "./components/preview.svelte";
-  import NewGasto from "./components/new-gasto.svelte";
+  import Preview from './components/preview.svelte'
+  import NewGasto from './components/new-gasto.svelte'
+  import { Router, Route, Link } from 'svero'
 </script>
 
-<Preview />
-<NewGasto />
+<nav>
+  <Link href="/">Home</Link>
+  <Link href="/gasto">Nuevo</Link>
+  <Link href="/dos">dos</Link>
+</nav>
+
+<Router>
+  <Route exact path="/" component={Preview} />
+  <Route path="/gasto">
+    <NewGasto nombre="asd" />
+  </Route>
+</Router>
