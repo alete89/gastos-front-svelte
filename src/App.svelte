f8a10c4f4324da885adc18d93ee7e39158c77466
alete89 <alejandrodini@mural.co>
2019-11-17 04:07:08 -0300

    feat: boilerplate for summary
    Co-authored-by: Nicolas Viotti <nicolasviotti90@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,23 @@
 <script>
   import Preview from './components/preview.svelte'
   import NewGasto from './components/new-gasto.svelte'
+  import Summary from './components/summary.svelte'
   import { Router, Route, Link } from 'svero'
 </script>
 
 <nav>
+  <Link href="/summary">Summary</Link>
   <Link href="/">Home</Link>
   <Link href="/gasto">Nuevo</Link>
 </nav>
 
 <Router>
   <Route exact path="/" component={Preview} />
-  <Route path="/gasto">
-    <NewGasto nombre="asd" />
-  </Route>
+  <Route path="/gasto" component={NewGasto} />
+  <Route path="/summary" component={Summary} />
 </Router>
+
+<!-- another way to route -->
+<!-- <Route path="/gasto">
+    <NewGasto />
+  </Route> -->
---
+++ src/components/summary.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { fetchTarjetas, fetchGastos } from '../services/appService.js'
+  import { fetchAnios } from '../services/monthService'
+  import { onMount } from 'svelte'
+
+  let tarjetas = []
+
+  onMount(async function() {
+    tarjetas = await fetchTarjetas()
+    anios = await fetchAnios(tarjeta)
+    await getGastos()
+  })
+
+  function currentMonth() {
+    return new Date().getMonth()
+  }
+</script>
+
+<div class="table-responsive">
+  <table class="table table-bordered table-hover">
+    <thead class="thead-dark">
+      <tr>
+        <th>Tarjeta</th>
+        <th>Mes pasado</th>
+        <th>Este Mes</th>
+        <th>Siguiente Mes</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each tarjetas as gasto, index}
+        <tr>
+          <th scope="row">{index + 1}</th>
+          <td>{0}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
