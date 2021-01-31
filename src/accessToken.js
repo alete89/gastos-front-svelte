cff7461ff973d101e6a488102a26a659a4464ad8
nicovio <nicolasviotti90@gmail.com>
2021-01-31 18:42:26 -0300

    fix: obtener el token de las cookies

--- src/accessToken.js
+++ src/accessToken.js
@@ -1,16 +1,3 @@
 import { writable } from 'svelte/store'
 
-const getCookieValue = (key) => {
-  const matchingCookies = document.cookie.match('(^|;)\\s*' + key + '\\s*=\\s*([^;]+)')
-  return matchingCookies?.pop()
-}
-
-export const accessToken = writable(getCookieValue('uid'))
-
-// export const setAccessToken = (newToken) => {
-//   accessToken = newToken
-// }
-
-// export const getAccessToken = () => {
-//   return accessToken
-// }
+export const accessToken = writable("")
